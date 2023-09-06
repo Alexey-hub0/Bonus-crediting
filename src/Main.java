d81331d76e57c1f91666b0d06fc19bc60a07100d
Alexey Test <yashin.aleksey.job@gmail.com>
2023-09-06 15:38:25 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int clientAccount = 100;
+        int addFunds = 1350;
+        int bonus;
+        if (addFunds >= 1000) {
+            bonus = addFunds / 100;
+        }
+        else {
+            bonus = 0;
+        }
+        System.out.println("Начислено " + bonus + " бонусов");
+        System.out.println("Итоговый счёт: " + (clientAccount + addFunds + bonus) + " руб");
     }
 }
